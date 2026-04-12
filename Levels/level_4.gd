c495961d40d06b622bf7f87e6a65a6523ecfada6
Simon <32257137+Invenblocker@users.noreply.github.com>
2026-04-12 14:10:27 +0200

    Changed Level 4 to make it less trivial

--- Levels/level_4.gd
+++ Levels/level_4.gd
@@ -7,24 +7,32 @@ func _ready() -> void:
 	%LevelScene.add_element(7, 0, HexTile.elements.FIRE, true)
 	%LevelScene.add_element(8, 3, HexTile.elements.FIRE, true)
 	%LevelScene.add_element(4, 2, HexTile.elements.FIRE, true)
-	%LevelScene.add_element(6, 7, HexTile.elements.FIRE, true)
-	%LevelScene.add_element(0, 1, HexTile.elements.FIRE, true)
-	%LevelScene.add_element(11, 4, HexTile.elements.FIRE, true)
-	%LevelScene.add_element(10, 5, HexTile.elements.FIRE, true)
+	%LevelScene.add_element(6, 7, HexTile.elements.FIRE, false)
+	%LevelScene.add_element(0, 1, HexTile.elements.FIRE, false)
+	%LevelScene.add_element(11, 4, HexTile.elements.FIRE, false)
+	%LevelScene.add_element(10, 5, HexTile.elements.FIRE, false)
 	%LevelScene.add_element(5, 6, HexTile.elements.FIRE, true)
+	%LevelScene.add_element(9, 4, HexTile.elements.FIRE, true)
+	%LevelScene.add_element(9, 6, HexTile.elements.FIRE, false)
+	%LevelScene.add_element(1, 5, HexTile.elements.FIRE, false)
+	%LevelScene.add_element(8, 0, HexTile.elements.FIRE, true)
+	%LevelScene.add_element(6, 0, HexTile.elements.FIRE, true)
+	%LevelScene.add_element(8, 7, HexTile.elements.FIRE, false)
+	%LevelScene.add_element(3, 6, HexTile.elements.FIRE, true)
+	%LevelScene.add_element(10, 6, HexTile.elements.FIRE, false)
 	%LevelScene.save_starting_state.emit()
 
 func _on_level_scene_post_step() -> void:
-	var anti_fire_count:int = 0
+	var remaining_fire = false
 	var tiles = %LevelScene.get_tile_map()
 	for row in tiles:
 		for tile in row:
 			if tile != null:
-				if (tile.element == HexTile.elements.FIRE) and (tile.antimatter == true):
-					anti_fire_count += 1
+				if (tile.element == HexTile.elements.FIRE):
+					remaining_fire = true
+					break
 	
-	print(anti_fire_count)
-	if anti_fire_count <= 0:
+	if !remaining_fire:
 		%ObjectiveLabel.visible = false
 		%CompleteLabel.visible = true
 		level_finished.emit()
